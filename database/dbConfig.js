155b907d180947e5861cb33b5e12284bd878a6be
Vyraal1 <kevin.afable@gmail.com>
2019-11-15 12:24:28 -0500

    todays guided prokect

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,5 +1,7 @@
-const knex = require('knex');
+const knex = require("knex");
 
-const knexConfig = require('../knexfile.js');
+const knexConfig = require("../knexfile.js");
+// const dbEnv = process.env.DB_ENV || "development";
 
+console.log("this is the dbEnv", dbEnv);
 module.exports = knex(knexConfig.development);
