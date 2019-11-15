cb9d24264c9696079599f876e41ac0cc9fb2a811
Vyraal1 <kevin.afable@gmail.com>
2019-11-15 12:32:34 -0500

    CHORE: dbConfig

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,7 +1,6 @@
 const knex = require("knex");
 
 const knexConfig = require("../knexfile.js");
-// const dbEnv = process.env.DB_ENV || "development";
+const dbEnv = process.env.DB_ENV || "development";
 
-console.log("this is the dbEnv", dbEnv);
-module.exports = knex(knexConfig.development);
+module.exports = knex(knexConfig[dbEnv]);
