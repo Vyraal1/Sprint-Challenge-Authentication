4fa646c363fa4cdf457aeef023e686366c03ee91
Vyraal1 <kevin.afable@gmail.com>
2019-11-15 12:03:13 -0500

    FEAT finished register and log in

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -19,7 +19,40 @@ router.post("/register", (req, res) => {
 });
 
 router.post("/login", (req, res) => {
-  // implement login
+  let { username, password } = req.body;
+
+  //search by username, since client doesn't know what their ID is
+  users
+    .findByUser({ username })
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = getJwtToken(user.username);
+        res.status(200).json({
+          message: "You're in the mainframe.",
+          token
+        });
+      } else {
+        res.status(401).json({ message: "Invalid credentials" });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: "Internal error when logging user in." });
+    });
 });
+
+function getJwtToken(username) {
+  const payload = {
+    username
+  };
+
+  const secret = process.env.JWT_SECRET || "There Is No Cow Level";
+
+  const options = {
+    expiresIn: "1d"
+  };
+
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
