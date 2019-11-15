f1f6a723db789ded655756a90fac89c4453c6d77
Vyraal1 <kevin.afable@gmail.com>
2019-11-15 11:35:28 -0500

    FEAT: Authentication middleware for JWT

--- api/auth/authenticate-middleware.js
+++ api/auth/authenticate-middleware.js
@@ -1,8 +1,16 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
-
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+  if (token) {
+    const secret = process.env.JWT_SECRET || "There Is No Cow Level";
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: "Invalid Credentials" });
+      } else {
+        req.decodedJwt = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(400).json({ message: "No credentials provided" });
+  }
 };
